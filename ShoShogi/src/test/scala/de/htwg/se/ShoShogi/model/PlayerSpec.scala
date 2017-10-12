c173e2aeeab5f094ef327580b06c36a2faa6bb5e
Mert Zeybek <me431zey@htwg-konstanz.de>
2017-10-12 14:54:17 +0200

    new

---
+++ ShoShogi/src/test/scala/de/htwg/se/ShoShogi/model/PlayerSpec.scala
@@ -0,0 +1,21 @@
+package de.htwg.se.ShoShogi.model
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class PlayerSpec extends WordSpec with Matchers {
+  "A Player" when {
+    "new" should {
+      val player = Player("Your Name")
+      "have a name" in {
+        player.name should be("Your Name")
+      }
+      "have a nice String representation" in {
+        player.toString should be("Your Name")
+      }
+    }
+  }
+
+}
