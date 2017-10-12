0e41c22454b9b3a17ece1a163cfefbd71fbb777a
Mert Zeybek <me431zey@htwg-konstanz.de>
2017-10-12 15:19:51 +0200

    test

---
+++ src/main/scala/de/htwg/se/ShoShogi/ShoShogi.scala
@@ -0,0 +1,14 @@
+package de.htwg.se.ShoShogi
+
+import de.htwg.se.ShoShogi.model.Player
+
+object ShoShogi {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+
+    /*
+    val student = Player("Your Name")
+    println("Hello, " + student.name)
+    */
+  }
+}
