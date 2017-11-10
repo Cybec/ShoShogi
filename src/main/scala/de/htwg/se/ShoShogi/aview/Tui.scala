0ee26e9123e25a5f0bf34bc78aaf00c16a081b4f
me431zey <me431zey@htwg-konstanz.de>
2017-11-10 11:15:28 +0100

    Ordnerstruktur aufgebaut

---
+++ src/main/scala/de/htwg/se/ShoShogi/aview/Tui.scala
@@ -0,0 +1,5 @@
+package de.htwg.se.ShoShogi.aview
+
+class Tui {
+
+}
--- src/main/scala/de/htwg/se/ShoShogi/model/Player.scala
+++ src/main/scala/de/htwg/se/ShoShogi/model/Player.scala
@@ -1,6 +1,5 @@
 package de.htwg.se.ShoShogi.model
 
-case class Player(name: String, first:Boolean) {
+case class Player(name: String, first: Boolean) {
   override def toString: String = name
 }
-
