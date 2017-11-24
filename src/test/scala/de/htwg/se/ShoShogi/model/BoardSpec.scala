feea27a4c0e34388fbf6c4298c8bb52b42880d8f
ni431str <nickstrehlee56@googlemail.com>
2017-11-24 10:50:37 +0100

    Board Test

--- src/test/scala/de/htwg/se/ShoShogi/model/BoardSpec.scala
+++ src/test/scala/de/htwg/se/ShoShogi/model/BoardSpec.scala
@@ -11,7 +11,7 @@ class BoardSpec extends WordSpec with Matchers {
       val smallBoard = new Board[Piece](1, new EmptyPiece)
       val biggerBoard = new Board[Piece](2, new EmptyPiece)
       val board = new Board[Piece](9, new EmptyPiece)
-      "be creted with the lenght of its edges as size. Testing size 1, 2 and 9" in {
+      "be created with the lenght of its edges as size. Testing size 1, 2 and 9" in {
         smallBoard.size should be(1)
         biggerBoard.size should be(2)
         board.size should be(9)
@@ -24,7 +24,7 @@ class BoardSpec extends WordSpec with Matchers {
         biggerBoard.cell(1, 1) should be(Some(biggerBoard.rows(1)(1)))
       }
     }
-    "a useable playingfield" should {
+    "using an actuall playfield" should {
       var board = new Board[Piece](9, new EmptyPiece)
       val player_1 = Player("Nick", true)
       board = board.replaceCell(0, 0, Lancer(player_1))
@@ -41,9 +41,25 @@ class BoardSpec extends WordSpec with Matchers {
       for (i <- 0 to 8) {
         board = board.replaceCell(i, 2, Pawn(player_1))
       }
-      "fields in uasble field" in {
-        board.cell(0, 0).contains(Lancer) should be(Lancer)
+      "replace cells with Pieces" in {
+        board.rows(0)(0) should be(Lancer(player_1))
+        board.rows(0)(1) should be(Knight(player_1))
+        board.rows(0)(2) should be(SilverGeneral(player_1))
+        board.rows(0)(3) should be(GoldenGeneral(player_1))
+        board.rows(0)(4) should be(King(player_1))
+        board.rows(0)(5) should be(GoldenGeneral(player_1))
+        board.rows(0)(6) should be(SilverGeneral(player_1))
+        board.rows(0)(7) should be(Knight(player_1))
+        board.rows(0)(8) should be(Lancer(player_1))
+        board.rows(1)(1) should be(Bishop(player_1))
+        board.rows(1)(7) should be(Rook(player_1))
+        board.rows(2)(0) should be(Pawn(player_1))
+        board.rows(2)(8) should be(Pawn(player_1))
       }
+      "have a nice String representation" in {
+
+      }
+
     }
   }
 }
