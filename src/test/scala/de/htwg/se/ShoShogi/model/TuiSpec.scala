6109513fa03d05deb3817cdb492e3a9b7ca0a8d1
ni431str <nickstrehlee56@googlemail.com>
2017-11-24 12:17:00 +0100

    TuiSpec

---
+++ src/test/scala/de/htwg/se/ShoShogi/model/TuiSpec.scala
@@ -0,0 +1,14 @@
+package de.htwg.se.ShoShogi.model
+
+import org.junit.runner.RunWith
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TuiSpec extends WordSpec with Matchers {
+  "A Tui" when {
+    "When Input User" should {
+
+    }
+  }
+}
