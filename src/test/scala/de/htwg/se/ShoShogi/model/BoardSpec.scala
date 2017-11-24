503fcdf83288209d4cb997a00bfaeaa350d638f3
ni431str <nickstrehlee56@googlemail.com>
2017-11-24 10:20:46 +0100

    Board Test

--- src/test/scala/de/htwg/se/ShoShogi/model/BoardSpec.scala
+++ src/test/scala/de/htwg/se/ShoShogi/model/BoardSpec.scala
@@ -13,15 +13,36 @@ class BoardSpec extends WordSpec with Matchers {
       val board = new Board[Piece](9, new EmptyPiece)
       "be creted with the lenght of its edges as size. Testing size 1, 2 and 9" in {
         smallBoard.size should be(1)
-        biggerBoard.size should be(4)
+        biggerBoard.size should be(2)
         board.size should be(81)
       }
       "give access to its cells" in {
-        smallBoard.cell(0, 0) should be(Some( ))
-        biggerBoard.cell(0, 0) should be(Some( ))
-        biggerBoard.cell(0, 1) should be(Some( ))
-        biggerBoard.cell(1, 0) should be(Some( ))
-        biggerBoard.cell(1, 1) should be(Some( ))
+        smallBoard.cell(0, 0) should be(Some(smallBoard.rows(0)(0)))
+        biggerBoard.cell(0, 0) should be(Some(biggerBoard.rows(0)(0)))
+        biggerBoard.cell(0, 1) should be(Some(biggerBoard.rows(0)(1)))
+        biggerBoard.cell(1, 0) should be(Some(biggerBoard.rows(1)(0)))
+        biggerBoard.cell(1, 1) should be(Some(biggerBoard.rows(1)(1)))
+      }
+    }
+    "a useable playingfield" should {
+      var board = new Board[Piece](9, new EmptyPiece)
+      val player_1 = Player("Nick", true)
+      board = board.replaceCell(0, 0, Lancer(player_1))
+      board = board.replaceCell(1, 0, Knight(player_1))
+      board = board.replaceCell(2, 0, SilverGeneral(player_1))
+      board = board.replaceCell(3, 0, GoldenGeneral(player_1))
+      board = board.replaceCell(4, 0, King(player_1))
+      board = board.replaceCell(5, 0, GoldenGeneral(player_1))
+      board = board.replaceCell(6, 0, SilverGeneral(player_1))
+      board = board.replaceCell(7, 0, Knight(player_1))
+      board = board.replaceCell(8, 0, Lancer(player_1))
+      board = board.replaceCell(1, 1, Bishop(player_1))
+      board = board.replaceCell(7, 1, Rook(player_1))
+      for (i <- 0 to 8) {
+        board = board.replaceCell(i, 2, Pawn(player_1))
+      }
+      "fields in uasble field" in {
+        board.cell(0, 0) should be(Some(board.rows(0)(0)))
       }
     }
   }
