ff06828f6c4badc140d432aaf04fd9c48b0c68df
ni431str <nickstrehlee56@googlemail.com>
2017-12-01 12:33:22 +0100

    TuiSpec

--- src/test/scala/de/htwg/se/ShoShogi/model/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/ShoShogi/model/ControllerSpec.scala
@@ -6,34 +6,36 @@ import de.htwg.se.ShoShogi.util.Observer
 
 import scala.language.reflectiveCalls
 import org.junit.runner.RunWith
-import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class ControllerSpec extends WordSpec with Matchers {
   "A Controller" when {
-    "observed by an Ovserver" should {
+    "" should {
       val controller = new Controller(new Board(boardSize, new EmptyPiece), Player("Player1", true), Player("Player2", false))
-      val observer = new Observer {
-        var updated: Boolean = false
-        def isUpdated: Boolean = updated
-        override def update: Unit = updated = true
-      }
-      controller.add(observer)
+
       "notify its Observer after creation of emptyBoard" in {
         controller.createEmptyBoard()
-        observer.updated should be(true)
         controller.board.size should be(9)
       }
       "notify its Observer after creation of playfield" in {
         controller.createNewBoard()
-        observer.updated should be(true)
         controller.board.size should be(9)
       }
       "remove from Observerlist" in {
         val i = controller.subscribers.length
-        controller.remove(observer)
         controller.subscribers.length should be(i - 1)
+      }
+      "printPossibleMoves called" should {
+        "print for \"pmv 0a\"" in {
+          val player_1 = Player("Player1", true)
+          controller.board = controller.board.replaceCell(0, 0, Lancer(player_1))
+          for (i <- 0 to 8) {
+            controller.board = controller.board.replaceCell(i, 2, Pawn(player_1))
+          }
+          controller.possibleMoves(0, 0) should be(List[(Int, Int)]((0, 1)))
+        }
       }
     }
   }
--- src/test/scala/de/htwg/se/ShoShogi/model/TuiSpec.scala
+++ src/test/scala/de/htwg/se/ShoShogi/model/TuiSpec.scala
@@ -8,16 +8,6 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TuiSpec extends WordSpec with Matchers {
-  "A Tui" when {
-    "when used" should {
-      val controller = new Controller(new Board(boardSize, new EmptyPiece), Player("Player1", true), Player("Player2", false))
-      val tui = new Tui(controller)
-      "print menu" in {
-       // val string = tui.printInputMenu()
-        //string should be("\n   0    1    2    3    4    5    6    7    8 \n \n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \ta\n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \tb\n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \tc\n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \td\n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \te\n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \tf\n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \tg\n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \th\n------------------------------------------------\n |    |    |    |    |    |    |    |    |    | \ti\n------------------------------------------------\nq: quit\n------\nn: new\n------")
-      }
-    }
-  }
   val boardSize = 9
   val controller = new Controller(new Board(boardSize, new EmptyPiece), Player("Player1", true), Player("Player2", false))
   val tui = new Tui(controller)
@@ -38,11 +28,6 @@ class TuiSpec extends WordSpec with Matchers {
       }
       "give back \"pmv     ca  \"" in {
         tui.parseArguments(Array("pmv", " ", " ", " ", " ", " ca", " ", " ")) should be(None)
-      }
-    }
-    "printPossibleMoves called" should {
-      "print for \"pmv 0a\"" in {
-        tui.printPossibleMoves(controller.possibleMoves(0, 0)) should be("Possible moves: (0, 1)   ")
       }
     }
   }
