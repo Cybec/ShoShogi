e43044a9ff9904a73e4d06e9ab6b1a4bf293c657
me431zey <me431zey@htwg-konstanz.de>
2018-01-15 19:10:52 +0100

    main travis playable

--- src/main/scala/de/htwg/se/ShoShogi/ShoShogi.scala
+++ src/main/scala/de/htwg/se/ShoShogi/ShoShogi.scala
@@ -1,13 +1,10 @@
 package de.htwg.se.ShoShogi
 
 import de.htwg.se.ShoShogi.aview.Tui
-import de.htwg.se.ShoShogi.controller.{ Controller, ControllerInterface, UpdateAll }
-import de.htwg.se.ShoShogi.model.{ Board, EmptyPiece, Player }
+import de.htwg.se.ShoShogi.controller.{ControllerInterface}
 import de.htwg.se.ShoShogi.aview.gui.SwingGui
-import de.htwg.se.ShoShogi.controller.{ Controller, UpdateAll }
-import de.htwg.se.ShoShogi.model.{ Board, EmptyPiece, Player }
-import de.htwg.se.ShoShogi.controller.{ UpdateAll, Controller }
-import de.htwg.se.ShoShogi.model.{ Board, EmptyPiece, Player }
+import de.htwg.se.ShoShogi.controller.{UpdateAll, Controller}
+import de.htwg.se.ShoShogi.model.{Board, EmptyPiece, Player}
 
 import scala.swing.Publisher
 
@@ -18,8 +15,8 @@ object ShoShogi extends Publisher {
     Player("Player1", true), Player("Player2", false)
   )
   val tui = new Tui(controller)
-  val gui = new SwingGui(controller)
-  listenTo(gui)
+  //  val gui = new SwingGui(controller)
+  //  listenTo(gui)
   controller.publish(new UpdateAll)
 
   def main(args: Array[String]): Unit = {
@@ -33,8 +30,8 @@ object ShoShogi extends Publisher {
     System.exit(0)
   }
 
-  reactions += {
-    case _ =>
-      if (gui == null) System.exit(0)
-  }
+  //  reactions += {
+  //    case _ =>
+  //      if (gui == null) System.exit(0)
+  //  }
 }
